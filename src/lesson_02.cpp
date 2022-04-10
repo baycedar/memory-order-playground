1c0e2b81c5fac498c5c2f61e0f12cbcaf3f66419
Kento Sugiura <sugiura@db.is.i.nagoya-u.ac.jp>
2022-04-10 10:02:08 +0900

    WIP: add lesson 2

--- src/lesson_02.cpp
+++ src/lesson_02.cpp
@@ -25,26 +25,24 @@
  * Global constants
  *####################################################################################*/
 
-constexpr size_t kThreadNum = 4;
 constexpr size_t kRepeatNum = 1e6;
 
 enum Exercise
 {
-  kWOAtomic,
-  kWithAtomic,
-  kWithCAS,
-  kWithFetchAdd
+  kWOFence,
+  kWithFence
 };
 
 /*######################################################################################
  * Global variables
  *####################################################################################*/
 
-/// a target memory address without std::atomic
-int64_t sum{0};
+struct Container {
+  size_t sum{0};
+};
 
 /// a target memory address with std::atomic
-std::atomic_int64_t atom_sum{0};
+std::atomic<Container *> ptr{nullptr};
 
 /*######################################################################################
  * Exercises
@@ -54,8 +52,9 @@ void
 AddWithFence()
 {
   for (size_t i = 0; i < kRepeatNum; ++i) {
-    sum += 1;
-    atom_sum.fetch_add(1, std::memory_order_release);
+    auto *target = ptr.load(std::memory_order_acquire);
+    target->sum += 1;
+    ptr.store(target, std::memory_order_release);
   }
 }
 
@@ -63,8 +62,9 @@ void
 AddWithoutFence()
 {
   for (size_t i = 0; i < kRepeatNum; ++i) {
-    sum += 1;
-    atom_sum.fetch_add(1, std::memory_order_relaxed);
+    auto *target = ptr.load(std::memory_order_relaxed);
+    target->sum += 1;
+    ptr.store(target, std::memory_order_relaxed);
   }
 }
 
@@ -72,13 +72,10 @@ void
 ReadWithFence()
 {
   while (true) {
-    const auto cur_atom_sum = atom_sum.load(std::memory_order_acquire);
-    const auto cur_sum = sum;
+    const auto *target = ptr.load(std::memory_order_acquire);
+    const auto cur_sum = target->sum;
 
-    std::cout << "sum w/o atomic : " << cur_sum << std::endl
-              << "sum with atomic: " << cur_atom_sum << std::endl
-              << std::endl;
-
+    std::cout << "sum: " << cur_sum << std::endl;
     if (cur_sum >= kRepeatNum) break;
   }
 }
@@ -87,13 +84,10 @@ void
 ReadWithoutFence()
 {
   while (true) {
-    const auto cur_atom_sum = atom_sum.load(std::memory_order_relaxed);
-    const auto cur_sum = sum;
+    const auto *target = ptr.load(std::memory_order_relaxed);
+    const auto cur_sum = target->sum;
 
-    std::cout << "sum w/o atomic : " << cur_sum << std::endl
-              << "sum with atomic: " << cur_atom_sum << std::endl
-              << std::endl;
-
+    std::cout << "sum: " << cur_sum << std::endl;
     if (cur_sum >= kRepeatNum) break;
   }
 }
@@ -119,18 +113,18 @@ main(  //
   std::cout << std::endl;
   const auto exe = static_cast<Exercise>(std::stoi(in));
 
+  // set up targets
+  ptr.store(new Container{0}, std::memory_order_release);
+
   // create worker threads for multi-threading
   std::thread reader{};
   std::thread writer{};
   switch (exe) {
-    case kWOAtomic:
-      std::cout << "wo" << std::endl;
+    case kWOFence:
       reader = std::thread{ReadWithoutFence};
       writer = std::thread{AddWithoutFence};
       break;
-
-    default:
-      std::cout << "with" << std::endl;
+    case kWithFence:
       reader = std::thread{ReadWithFence};
       writer = std::thread{AddWithFence};
       break;
@@ -139,6 +133,9 @@ main(  //
   // wait for the worker threads to complete their jobs
   writer.join();
   reader.join();
+
+  // tear down  targets
+  delete ptr.load(std::memory_order_relaxed);
 
   return 0;
 }
